91955282b78508e595e0f0aeec6bb28015076dd3
Jacob Bednarz <jacob.bednarz@gmail.com>
2013-09-30 06:57:55 +1000

    Broke SSH tasks out into own class

--- lib/acquia_toolbelt/cli/ssh.rb
+++ lib/acquia_toolbelt/cli/ssh.rb
@@ -1,9 +1,40 @@
 module AcquiaToolbelt
   class CLI
     class SSH < AcquiaToolbelt::Thor
-      desc "example", "example ssh command"
-      def example
-        puts "example ssh command"
+      # Public: List all SSH users.
+      #
+      # Returns a list of users and their SSH keys truncated.
+      desc "list", "List all SSH users."
+      def list
+        if options[:subscription]
+          subscription = options[:subscription]
+        else
+          subscription = AcquiaToolbelt::CLI::API.default_subscription
+        end
+
+        users = AcquiaToolbelt::CLI::API.request "sites/#{subscription}/sshkeys"
+        users.each do |user|
+          say
+          say "> ID: #{user["id"]}"
+          say "> Name: #{user["nickname"]}"
+          say "> Key: #{ AcquiaToolbelt::CLI::API.truncate_ssh_key user["ssh_pub_key"]}"
+        end
+      end
+
+      desc "delete", "Delete a SSH key."
+      method_option :id, :type => :string, :aliases => %w(-i), :required => true,
+        :desc => "User ID to delete from SSH users."
+      def delete
+        if options[:subscription]
+          subscription = options[:subscription]
+        else
+          subscription = AcquiaToolbelt::CLI::API.default_subscription
+        end
+
+        id = options[:id]
+
+        delete_ssh_request = AcquiaToolbelt::CLI::API.request "sites/#{subscription}/sshkeys/#{id}", "DELETE"
+        ui.success "SSH key #{id} has been successfully deleted." if delete_ssh_request["id"]
       end
     end
   end
