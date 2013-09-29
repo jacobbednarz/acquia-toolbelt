21c3a4930193a03063a0c6c3c77e57f6beef6d2c
Jacob Bednarz <jacob.bednarz@gmail.com>
2013-09-29 10:09:47 +1000

    Moved over site listing to new classy style

--- lib/acquia_toolbelt/site.rb
+++ lib/acquia_toolbelt/site.rb
@@ -1,8 +1,23 @@
 module AcquiaToolbelt
   class Site < AcquiaToolbelt::Thor
-    desc "example", "example site command"
-    def example
-      puts "example site command"
+    desc "list", "List all subscriptions you have access to."
+    def list
+      sites = AcquiaToolbelt::CLI::API.request "sites"
+
+      sites.each do |site|
+        ui.say
+        # Get the individual subscription information.
+        site = AcquiaToolbelt::CLI::API.request "sites/#{site}"
+
+        ui.say "#{site["title"]}"
+        ui.say "> Username: #{site["unix_username"]}"
+        ui.say "> Subscription: #{site["name"]}"
+
+        # If the VCS type is SVN, we want it in all uppercase, otherwise just
+        # capitilise it.
+        vcs_name = (site["vcs_type"] == "svn") ? site["vcs_type"].upcase : site["vcs_type"].capitalize
+        ui.say "> #{vcs_name} URL: #{site["vcs_url"]}"
+      end
     end
   end
 end
