9e60247f5917c5c5cd505ce45e3a1eb0fb6c2694
Jacob Bednarz <jacob.bednarz@gmail.com>
2013-09-29 12:00:05 +1000

    Ported code deploy over

--- lib/acquia_toolbelt/cli/deploy.rb
+++ lib/acquia_toolbelt/cli/deploy.rb
@@ -1,9 +1,21 @@
 module AcquiaToolbelt
   class CLI
     class Deploy < AcquiaToolbelt::Thor
-      desc "example", "example deploy command"
-      def example
-        puts "example deploy command"
+      desc "code", "Deploy a VCS branch or tag to an environment."
+      method_option :release, :type => :string, :aliases => %w(-r), :required => true,
+        :desc => "Name of the release to deploy to the environment."
+      def code
+        if options[:environment].nil?
+          ui.say "No value provided for required options '--environment'"
+          return
+        end
+
+        subscription = options[:subscription] ? options[:subscription] : AcquiaToolbelt::CLI::API.default_subscription
+        environment  = options[:environment]
+        release      = options[:release]
+
+        deploy_code =  AcquiaToolbelt::CLI::API.request "sites/#{subscription}/envs/#{environment}/code-deploy", "CODE-DEPLOY-POST", :release => "#{release}"
+        ui.success "#{release} has been deployed to #{environment}." if deploy_code["id"]
       end
     end
   end
