ef9201a14e10901e59e635d4077562226c2e6177
Jacob Bednarz <jacob.bednarz@gmail.com>
2013-09-29 10:10:26 +1000

    Ported over domain listing for subscriptions

--- lib/acquia_toolbelt/domain.rb
+++ lib/acquia_toolbelt/domain.rb
@@ -1,20 +1,34 @@
 module AcquiaToolbelt
-  class Domain < AcquiaToolbelt::Thor
-    desc "example", "example domain command"
-    def example
-      puts "example domain command"
+  class CLI
+    class Domain < AcquiaToolbelt::Thor
+      desc "list", "List all domains."
+      def list
+        # Set the subscription if it has been passed through, otherwise use the
+        # default.
+        if options[:subscription]
+          site = options[:subscription]
+        else
+          site = AcquiaToolbelt::CLI::API.default_subscription
+        end
+
+        # Get all the environments to loop over unless the environment is set.
+        if options[:environment]
+          environments = []
+          environments << options[:environment]
+        else
+          environments = AcquiaToolbelt::CLI::API.get_environments
+        end
+
+        environments.each do |environment|
+          domains = AcquiaToolbelt::CLI::API.request "sites/#{site}/envs/#{environment}/domains"
+          ui.say
+          ui.say "Environment: #{environment}" unless options[:environment]
+
+          domains.each do |domain|
+            ui.say "> #{domain["name"]}"
+          end
+        end
+      end
     end
-
-    desc "list", "List all domains."
-    def list
-      sites = AcquiaToolbelt::CLI::API.acquia_api_call("sites")
-      ui.info sites
-    end
-
-    # def add
-    # end
-
-    # def delete
-    # end
   end
 end
