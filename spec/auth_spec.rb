2617cb5c756cdc077c3f1f95a7dc8a73527cfbf1
Jacob Bednarz <jacob.bednarz@gmail.com>
2013-11-05 18:55:46 +1000

    Updated netrc spec description wording

--- spec/auth_spec.rb
+++ spec/auth_spec.rb
@@ -1,6 +1,6 @@
 require "netrc"
 
-describe "when reading netrc values" do
+describe "when using netrc for authentication" do
   it "should be able to read login values" do
     n = Netrc.read
     assert n["cloudapi.acquia.com"], "login values cannot be read or do not exist."
